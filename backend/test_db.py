f3103f7217bbd3aaf3e571407f0c2c749125ced0
sasirekhaRavi2005 <sasirekharavi2005@gmail.com>
2025-12-21 13:05:31 +0530

    Added db

---
+++ backend/test_db.py
@@ -0,0 +1,10 @@
+from db import (
+    insert_product,
+    get_product,
+    insert_review,
+    get_reviews,
+    insert_sentiment,
+    get_sentiment_summary
+)
+
+print("MongoDB connection successful!")
